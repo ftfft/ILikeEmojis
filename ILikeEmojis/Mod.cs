33d1c100ad103a50f47567d1afef855b5a95cf4c
ftfft <97695624+ftfft@users.noreply.github.com>
2022-04-22 10:28:03 -0700

    make the button work

--- ILikeEmojis/Mod.cs
+++ ILikeEmojis/Mod.cs
@@ -1,5 +1,6 @@
 ﻿using MelonLoader;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using VRC.UI.Elements.Menus;
 
@@ -13,7 +14,6 @@ namespace ILikeEmojis
 
         private static GameObject emojiButton;
         private static GameObject InteractionPauseButton;
-
         public override void OnApplicationStart() {
             Category = MelonPreferences.CreateCategory("ILikeEmojis");
             EnableEmojiButton = Category.CreateEntry("EnableEmojiButton", false);
@@ -27,12 +27,15 @@ namespace ILikeEmojis
             GameObject ro = SearchNonActive(rd, "ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickActions/");
             emojiButton = SearchNonActive(ro, "Button_Emojis");
             InteractionPauseButton = SearchNonActive(ro, " Button_InteractionPauseWithState");
-            emojiButton.active = EnableEmojiButton.Value;
             InteractionPauseButton.active = !DisableInteractionPauseButton.Value;
             EnableEmojiButton.OnValueChanged += (bool o, bool n) => emojiButton.active = n;
             DisableInteractionPauseButton.OnValueChanged += (bool o, bool n) => InteractionPauseButton.active = !n;
-            rd.GetComponent<LaunchPadQMMenu>().field_Public_Button_6 = null;
+            LaunchPadQMMenu lm = rd.GetComponent<LaunchPadQMMenu>();
+            while (lm.field_Public_Button_6?.onClick?.m_Calls?.Count == 0) yield return null;
+            lm.field_Public_Button_6 = null;
+            emojiButton.active = EnableEmojiButton.Value;
         }
+
         private GameObject SearchNonActive(GameObject root, string path) => Transform.FindRelativeTransformWithPath(root.transform, path, false).gameObject;
     }
 }
