d01c4455c660b9a2782bb377ecd5b362e9d9c5bc
ftfft <97695624+ftfft@users.noreply.github.com>
2022-04-21 23:22:26 -0700

    init

---
+++ ILikeEmojis/Mod.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System.Collections;
+using VRC.UI.Elements.Menus;
+
+namespace ILikeEmojis
+{
+    public class Mod : MelonMod
+    {
+        public static MelonPreferences_Category Category;
+        public static MelonPreferences_Entry<bool> EnableEmojiButton;
+        public static MelonPreferences_Entry<bool> DisableInteractionPauseButton;
+
+        private static GameObject emojiButton;
+        private static GameObject InteractionPauseButton;
+
+        public override void OnApplicationStart() {
+            Category = MelonPreferences.CreateCategory("ILikeEmojis");
+            EnableEmojiButton = Category.CreateEntry("EnableEmojiButton", false);
+            DisableInteractionPauseButton = Category.CreateEntry("DisableInteractionPauseButton", false);
+            MelonCoroutines.Start(WaitForUiManagerInit());
+        }
+
+        private IEnumerator WaitForUiManagerInit() {
+            while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
+            GameObject rd = SearchNonActive(GameObject.Find("UserInterface"), "Canvas_QuickMenu(Clone)/Container/Window/QMParent/Menu_Dashboard/");
+            GameObject ro = SearchNonActive(rd, "ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickActions/");
+            emojiButton = SearchNonActive(ro, "Button_Emojis");
+            InteractionPauseButton = SearchNonActive(ro, " Button_InteractionPauseWithState");
+            emojiButton.active = EnableEmojiButton.Value;
+            InteractionPauseButton.active = !DisableInteractionPauseButton.Value;
+            EnableEmojiButton.OnValueChanged += (bool o, bool n) => emojiButton.active = n;
+            DisableInteractionPauseButton.OnValueChanged += (bool o, bool n) => InteractionPauseButton.active = !n;
+            rd.GetComponent<LaunchPadQMMenu>().field_Public_Button_6 = null;
+        }
+        private GameObject SearchNonActive(GameObject root, string path) => Transform.FindRelativeTransformWithPath(root.transform, path, false).gameObject;
+    }
+}
---
+++ ILikeEmojis/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using ILikeEmojis;
+
+[assembly: MelonInfo(typeof(Mod), "I Like Emojis", "1.0.0", "ftfft", "Http://www.github.com/ftfft/ILikeEmojis")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ILikeEmojis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ILikeEmojis")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33d10a5d-32f0-49ba-a465-e3ac667f547b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
