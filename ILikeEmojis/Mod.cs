41b374be107ea3fee2c023889f1898af6fb55217
ftfft <97695624+ftfft@users.noreply.github.com>
2022-06-21 04:10:47 -0700

    ver 1.0.3

--- ILikeEmojis/Mod.cs
+++ ILikeEmojis/Mod.cs
@@ -1,7 +1,10 @@
 ﻿using MelonLoader;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using VRC.UI.Elements.Menus;
+using System.Linq;
+using UnityEngine.SceneManagement;
 
 namespace ILikeEmojis
 {
@@ -13,22 +16,53 @@ namespace ILikeEmojis
 
         private static GameObject emojiButton;
         private static GameObject InteractionPauseButton;
-        public override void OnApplicationStart() {
+
+        public override void OnApplicationStart()
+        {
             Category = MelonPreferences.CreateCategory("ILikeEmojis");
             EnableEmojiButton = Category.CreateEntry("EnableEmojiButton", true);
             DisableInteractionPauseButton = Category.CreateEntry("DisableInteractionPauseButton", true);
+
+
+
             MelonCoroutines.Start(WaitForUiManagerInit());
+
+
         }
 
-        private IEnumerator WaitForUiManagerInit() {
+        private IEnumerator WaitForUiManagerInit()
+        {
+            yield return new WaitForEndOfFrame();
+
+            List<GameObject> prefabs = Prefabs();
+
+            GameObject qm_prefab = prefabs.First((ob) => ob.name == "Canvas_QuickMenu");
+
+            GameObject qmw = SearchNonActive(qm_prefab, "Container/Window");
+            GameObject qmp = SearchNonActive(qmw, "QMParent");
+            
+            Sprite ei = SearchNonActive(qmw, "Wing_Left/Container/InnerContainer/WingMenu/ScrollRect/Viewport/VerticalLayoutGroup/Button_Emoji/Container/Icon").GetComponent<UnityEngine.UI.Image>().sprite;
+
+            SearchNonActive(qmp, "Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickActions/Button_Emojis/Icon").GetComponent<UnityEngine.UI.Image>().sprite = ei;
+
+            Sprite ai = SearchNonActive(qmp, "Menu_Here/ScrollRect/Viewport/VerticalLayoutGroup/QM_Foldout_WorldActions/Arrow").GetComponent<UnityEngine.UI.Image>().sprite;
+            GameObject qmgf_prefab = prefabs.First((ob) => ob.name == "QM_GridFoldout");
+
+
+            SearchNonActive(qmgf_prefab, "Arrow").GetComponent<UnityEngine.UI.Image>().sprite = ai;
+
+
             while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
-            GameObject rd = SearchNonActive(GameObject.Find("UserInterface"), "Canvas_QuickMenu(Clone)/Container/Window/QMParent/Menu_Dashboard/");
+            GameObject rw = SearchNonActive(GameObject.Find("UserInterface"), "Canvas_QuickMenu(Clone)/Container/Window/");
+            GameObject rp = SearchNonActive(rw, "QMParent");
+            GameObject rd = SearchNonActive(rp, "Menu_Dashboard");
             GameObject ro = SearchNonActive(rd, "ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickActions/");
             emojiButton = SearchNonActive(ro, "Button_Emojis");
             InteractionPauseButton = SearchNonActive(ro, " Button_InteractionPauseWithState");
             InteractionPauseButton.active = !DisableInteractionPauseButton.Value;
             EnableEmojiButton.OnValueChanged += (bool o, bool n) => emojiButton.active = n;
             DisableInteractionPauseButton.OnValueChanged += (bool o, bool n) => InteractionPauseButton.active = !n;
+
             LaunchPadQMMenu lm = rd.GetComponent<LaunchPadQMMenu>();
             while (lm.field_Public_Button_6?.onClick?.m_Calls?.Count == 0) yield return null; // wait for the button to get its onClick assigned
             lm.field_Public_Button_6 = null;
@@ -36,5 +70,16 @@ namespace ILikeEmojis
         }
 
         private GameObject SearchNonActive(GameObject root, string path) => Transform.FindRelativeTransformWithPath(root.transform, path, false).gameObject;
+
+        private List<GameObject> Prefabs()
+        {
+            List<GameObject> ret = new List<GameObject>();
+            foreach (GameObject go in Resources.FindObjectsOfTypeAll<GameObject>())
+            {
+                if (go.transform.parent == null && !go.scene.IsValid() && (go.name == "Canvas_QuickMenu" || go.name == "QM_GridFoldout"))
+                    ret.Add(go);
+            }
+            return ret;
+        }
     }
 }
